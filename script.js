15d82f972da9dc2d2457527d0d9a109b94ad6639
Real-Artisan <iwenyadaniel12@gmail.com>
2022-03-17 19:36:13 +0100

    so far

--- script.js
+++ script.js
@@ -14,3 +14,82 @@ function toggleSignup()
     unit.style.display = "none"
  }
 }
+
+//toggle upload
+function toggleUpload()
+{
+  let upload = document.getElementById("drop-here");
+  if(upload.style.display !== "grid")
+  {
+    upload.style.display = "grid"
+  }
+  else
+  {
+    upload.style.display = "none"
+  }
+}
+//toggle Nav Bar
+function toggleNavBar()
+{
+  let data = document.getElementById("sidebar");
+  if(data.style.display !== "block")
+  {
+    data.style.display = "block"
+  }
+  else
+  {
+    data.style.display = "none"
+  }
+
+}
+
+
+// drag and drop upload
+let dropSpace = document.getElementById("drop-here");
+let data = ['dragenter', 'dragover', 'dragleave', 'drop'];
+data.forEach(eventName => {
+   dropSpace.addEventListener(eventName, preventDefaults, false)
+ })
+ 
+ function preventDefaults (e) {
+   e.preventDefault()
+   e.stopPropagation()
+ }
+ 
+ ['dragleave', 'drop'].forEach(eventName => {
+   dropSpace.addEventListener(eventName, unhighlight, false)
+ })
+ 
+ function highlight(e) {
+   dropSpace.classList.add('highlight')
+ }
+ 
+ function unhighlight(e) {
+   dropSpace.classList.remove('highlight')
+ }
+
+ dropSpace.addEventListener('drop', handleDrop, false)
+
+ function handleDrop(e) {
+   let dt = e.dataTransfer
+   let files = dt.files
+ 
+   fileEvent(files)
+ }
+
+ function fileEvent(files) {
+   ([...files]).forEach(uploadFile)
+ }
+ function uploadFile(file) {
+   let url = 'YOUR URL HERE'
+   let formData = new FormData()
+ 
+   formData.append('file', file)
+ 
+   fetch(url, {
+     method: 'POST',
+     body: formData
+   })
+   .then(() => { /* Done. Inform the user */ })
+   .catch(() => { /* Error. Inform the user */ })
+ }
