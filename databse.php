9fd70e6c40419946520399bc3f71cb85928dbdb3
Real-Artisan <iwenyadaniel12@gmail.com>
2022-03-19 08:34:31 +0100

    pgconnect

--- databse.php
+++ databse.php
@@ -1,10 +1,11 @@
 <?php
 $dbHost = "app-e16974f1-49df-4ff7-854b-4a18ee4f9ed4-do-user-11129028-0.b.db.ondigitalocean.com" ;
 $dbUser = "scrapdb" ;
+$dbPort = "25060" ;
 $dbPass = "kUruFa19KXtojBrC" ;
 $dbName = "scrapdb" ;
 
-if(!$db = pg_connect($dbHost,$dbUser,$dbPass,$dbName))
+if(!$db = pg_connect("host=`$dbHost` posrt=`$dbPort`dbname=`$dbName` user=`$dbUser` password=`$dbPass` connect_timeout=10"))
 {
         echo ("failed attempt to connect!");
         die;
